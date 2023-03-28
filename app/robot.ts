96470a0bebe35559a1d19ebb1c28ee1fa2dfefd6
Eduardo Azuri Gaytán Martínez <azuri.1994@gmail.com>
2023-03-28 11:22:34 -0500

    feat: adding sitemap

---
+++ app/robot.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://azuri.app/sitemap.xml',
+    host: 'https://azuri.app',
+  }
+}
