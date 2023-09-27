9a8a0c1d861130eb17297528bf1853cb5214bf5e
Eduardo Azuri Gaytán Martínez <azuri.1994@gmail.com>
2023-09-27 14:53:17 -0500

    fix: fixing ts errors :shame

--- components/MagneticWrapper.tsx
+++ components/MagneticWrapper.tsx
@@ -1,22 +1,25 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { useRef, useState } from 'react'
+import { type RefObject, useRef, useState } from 'react'
 
 export default function MagneticWrapper({
   children,
 }: {
   children: React.ReactNode
 }) {
-  const ref = useRef(null)
+  const ref: RefObject<HTMLDivElement> = useRef(null)
   const [position, setPosition] = useState({ x: 0, y: 0 })
 
-  const mouseMove = (e) => {
+  const mouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
     const { clientX, clientY } = e
-    const { width, height, left, top } = ref.current?.getBoundingClientRect()
-    const x = clientX - (left + width / 2)
-    const y = clientY - (top + height / 2)
-    setPosition({ x, y })
+    const rect = ref.current?.getBoundingClientRect()
+    if (rect) {
+      const { width, height, left, top } = rect
+      const x = clientX - (left + width / 2)
+      const y = clientY - (top + height / 2)
+      setPosition({ x, y })
+    }
   }
 
   const mouseLeave = () => {
